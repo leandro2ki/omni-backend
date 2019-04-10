0084927be00d2f462bd216789e88f21a181e6c5d
Leandro Kimura <leandro2ki@gmail.com>
2019-04-10 15:48:48 -0300

    Estrutura

---
+++ src/config/multer.js
@@ -0,0 +1,23 @@
+const multer = require('multer')
+const path = require('path')
+const crypto = require('crypto')
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', 'tmp'),
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.resolve(__dirname, '..', 'tmp'))
+        },
+        filename: (req, file, cb) => {
+            //gerar 16 bits de chars aleatorios
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err);
+                // 123ho1iu23u33-test.jpg
+                file.key = `${hash.toString('hex')}-${file.originalname}`
+
+                cb(null, file.key)
+            })
+        }
+    })
+
+}
---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path')
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors)
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+//qdo receber a conexao do front
+io.on('connection', socket => {
+    //quando receber a conexao connectRoom
+    socket.on('connectRoom', box => {
+        //entra na sala box
+        socket.join(box);
+    })
+})
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-xjll8.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true,
+})
+
+//middleware para acessar o io
+app.use((req, res, next) => {
+    req.io = io;
+
+    return next();
+})
+
+
+app.use(express.json());
+//permite envio de arquivos
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/files', express.static(path.resolve(__dirname, 'tmp')))
+
+app.use(require('./routes'));
+
+server.listen(3333)
+// app.listen(3333);
