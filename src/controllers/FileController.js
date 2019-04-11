f26c5425ec91efb56496766f7e3a04890c75ea1c
Leandro Kimura <leandro2ki@gmail.com>
2019-04-11 15:39:19 -0300

    Fixing File controller socket

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,28 +1,26 @@
-const File = require('../models/File')
-const Box = require('../models/Box')
-
+const File = require("../models/File");
+const Box = require("../models/Box");
 
 class FileController {
-    async store (req, res ) {
-       // Criar  um arquivo
-       const box = await Box.findById(req.params.id);
-       const file = await File.create({
-           title: req.file.originalname,
-           path: req.file.key,
-       })
+  async store(req, res) {
+    // Criar  um arquivo
+    const box = await Box.findById(req.params.id);
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    });
 
-       box.files.push(file)
+    box.files.push(file);
 
-       await box.save()
+    await box.save();
 
-       // avisar qd receber o arquivo adicionado
-       req.io.socket.in(box._id).emit('file', file)
+    // avisar qd receber o arquivo adicionado
+    req.io.sockets.in(box._id).emit("file", file);
 
-
-       return res.json(file)
-        // console.log(req.file)
-        // return res.send('ok')
-    }
+    return res.json(file);
+    // console.log(req.file)
+    // return res.send("ok");
+  }
 }
 
 module.exports = new FileController();
